daadcf5acd07041b8acb8b375ab8d50c70bd6905
Antonio072 <16030681@itcelaya.edu.mx>
2021-11-16 23:31:05 -0600

    feat: Add dynamic elements search

---
+++ dynamic_elements.py
@@ -0,0 +1,46 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class DynamicElements(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+        driver = self.driver
+        driver.get("http://the-internet.herokuapp.com")
+        driver.maximize_window()
+        driver.find_element_by_link_text("Disappearing Elements").click()
+
+    def test_name_elements(self):
+        driver = self.driver
+
+        options = []
+        menu = 5
+        tries = 1
+
+        while len(options) < 5:
+            options.clear()
+
+            for i in range(menu):
+                try:
+                    option_name = driver.find_element_by_xpath(f'/html/body/div[2]/div/div/ul/li[{ i + 1}]/a')
+                    options.append(option_name.text)
+                    print(options)
+
+                except Exception as e:
+                    print(f"Option number {i + 1} is not found")
+                    tries += 1
+                    driver.refresh()
+
+        print(f'FInished in {tries} tries')
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2, testRunner = HTMLTestRunner(output="reportes", report_name= "Dynamic Elements"))
